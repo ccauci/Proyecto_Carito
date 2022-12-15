01596ce10b37905cae0e917b6ba28a0ba59aa9f3
ccauci <carolina.caucino@gmail.com>
2022-12-15 00:33:27 -0300

    .

--- Proyecto_Car/src/app/app.module.ts
+++ Proyecto_Car/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
